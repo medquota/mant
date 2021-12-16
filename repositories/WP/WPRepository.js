b17548d8d969e0fcc2f0b36c3f04665bafbbc0dc
medquota <87965303+medquota@users.noreply.github.com>
2021-12-16 17:20:48 +0100

    Update WPRepository.js

--- repositories/WP/WPRepository.js
+++ repositories/WP/WPRepository.js
@@ -4,7 +4,7 @@
  * */
 import axios from 'axios';
 
-export const WPDomain = 'https://medquota.com';
+export const WPDomain = 'https://www.naturalpetcare.us';
 
 const username = 'ck_38ea6b13f8204fcd94665ba180c939e505c2f5e7';
 const password = 'cs_ff4c6db8ce7e05f74f569d4b60462ab1016b9f2d';
