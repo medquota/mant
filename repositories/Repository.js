1fe9bfb6deec39ccce23197d0839b15ea992a443
medquota <87965303+medquota@users.noreply.github.com>
2021-12-16 17:21:11 +0100

    Update Repository.js

--- repositories/Repository.js
+++ repositories/Repository.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const baseDomain = 'http://45.76.97.89:3000';
-export const wp = 'https://medquota.com';
+export const wp = 'https://www.naturalpetcare.us';
 //export const wp = 'http://wp.diaryforlife.info';
 
 const authorization_prefix = 'Bearer ';
